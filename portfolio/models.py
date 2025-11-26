64108b89c429ed1e2dd74e923684be1fa4170bd8
Buya96 <ayub1231@hotmail.com>
2025-11-26 16:49:37 +0000

    add holding model linking portfolio and cryptocurrency

--- portfolio/models.py
+++ portfolio/models.py
@@ -12,3 +12,12 @@ class Portfolio(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s Portfolio"
+
+class Holding(models.Model):
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE),
+    cryptocurrency = models.ForeignKey(Cryptocurrency, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=20, decimal_places=8)
+    average_purchase_price = models.DecimalField(max_digits=20, decimal_places=8)
+
+    def __str__(self):
+        return f"{self.cryptocurrency.name}: {self.amount}"
