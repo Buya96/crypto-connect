d37eaedeff0803077b1e83d9c2c48ce07a559733
Buya96 <ayub1231@hotmail.com>
2025-11-26 16:25:41 +0000

    portfolio model with one to one user link

--- portfolio/models.py
+++ portfolio/models.py
@@ -6,3 +6,9 @@ class Cryptocurrency (models.Model):
 
     def __str__(self):
         return self.name 
+
+class Portfolio(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username}'s Portfolio"
