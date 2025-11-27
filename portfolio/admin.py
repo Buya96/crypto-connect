b121b3abfc3e12c9e528f97ce343d47132cafce3
Buya96 <ayub1231@hotmail.com>
2025-11-27 17:42:40 +0000

    edit admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Cryptocurrency, Portfolio, Holding, Transaction
+from .models import Cryptocurrency, Portfolio, Holding
+
 
 
 # Register your models here.
 admin.site.register(Cryptocurrency)
 admin.site.register(Portfolio)
 admin.site.register(Holding)
-admin.site.register(Transaction)
