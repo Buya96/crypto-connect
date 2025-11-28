85f6907314d53438abadb27868db474dee250cc8
Buya96 <ayub1231@hotmail.com>
2025-11-28 17:48:30 +0000

    add portfolio view for logged in user

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import Portfolio
 
 # Create your views here.
+@login_required
+def my_portfolio(request):
+    portfolio = Portfolio.objects.get(user=request.user)
+    return render(request, "portfolio/my_portfolio.html", {"portfolio": portfolio})
