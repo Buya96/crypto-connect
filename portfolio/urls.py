3217ef595a2b752e2c5586a0c0848d4ee9d9e529
Buya96 <ayub1231@hotmail.com>
2025-11-29 16:45:53 +0000

    root URL to profile

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path("my-portfolio/",views.my_portfolio, name="my_portfolio"),  
-]   
+    path("", views.my_portfolio, name="home"),           # ← Maps "/"
+    path("my-portfolio/", views.my_portfolio, name="my_portfolio"),
+]
