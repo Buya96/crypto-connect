045418051eb69bdc6feed110a2733f3900400103
Buya96 <ayub1231@hotmail.com>
2025-11-29 17:03:56 +0000

    fix missing portfolio issue

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.contrib import messages 
 from .models import Portfolio
 
 # Create your views here.
 @login_required
 def my_portfolio(request):
-    portfolio = Portfolio.objects.get(user=request.user)
-    return render(request, "portfolio/my_portfolio.html", {"portfolio": portfolio})
+    try:
+        portfolio = Portfolio.objects.get(user=request.user)
+    except Portfolio.DoesNotExist:
+        messages.error(request, "Portfolio not found.")
+        portfolio = None
+    return render(request, 'portfolio/my_portfolio.html', {'portfolio': portfolio}) 
