7339a601814b9ec90bb4d63d6070f61122685548
Buya96 <ayub1231@hotmail.com>
2025-11-29 22:18:35 +0000

    add holding url

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.my_portfolio, name="home"),           # ← Maps "/"
     path("my-portfolio/", views.my_portfolio, name="my_portfolio"),
+    path("add-holding/", views.add_holding, name="add_holding"),
 ]
