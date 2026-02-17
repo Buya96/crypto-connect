a38f3931b39c1df4acc80904b07162a7e1c8ec16
Buya96 <ayub1231@hotmail.com>
2026-02-17 18:46:05 +0000

    signup url

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("add-holding/", views.add_holding, name="add_holding"),
     path("edit-holding/<int:holding_id>/", views.edit_holding, name="edit_holding"),
     path("delete-holding/<int:holding_id>/", views.delete_holding, name="delete_holding"),
+    path("signup/", views.signup, name="signup"),
 ]
 
